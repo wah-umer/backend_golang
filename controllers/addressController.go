a5292159f2c48b2843caefe9c12f57ec3ee6cbd0
wah-umer <umer.waheed@devsinc.com>
2025-05-21 00:15:50 +0500

    Created Address Managment API's

---
+++ controllers/addressController.go
@@ -0,0 +1,160 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/umerwaheed/backend_golang/database"
+	models "github.com/umerwaheed/backend_golang/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var addrColl = database.Collection("addresses")
+
+func GetByUserId(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+	params := mux.Vars(r)
+	addrId := params["id"]
+
+	id, err := primitive.ObjectIDFromHex(addrId)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	filter := bson.M{"user": id}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cur, err := addrColl.Find(ctx, filter)
+
+	if err != nil {
+		log.Println("DB error on Find:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(ctx)
+
+	//decode the address into slices
+	var addresses []models.Address
+	if err := cur.All(ctx, &addresses); err != nil {
+		log.Println("Cursor.All error:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(addresses)
+}
+
+func UpdateById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+
+	params := mux.Vars(r)
+	addrId := params["id"]
+
+	id, err := primitive.ObjectIDFromHex(addrId)
+	if err != nil {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var updates map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&updates); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+	//prevent update to user id or id
+	delete(updates, "_id")
+	delete(updates, "user")
+
+	update := bson.M{"$set": updates}
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
+
+	var updated models.Address
+	err = addrColl.FindOneAndUpdate(ctx, bson.M{"_id": id}, update, opts).Decode(&updated)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, "Address not found", http.StatusNotFound)
+			return
+		}
+		log.Println("DB error on Update:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updated)
+}
+
+func DeleteById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+
+	params := mux.Vars(r)
+	addrID := params["id"]
+
+	id, err := primitive.ObjectIDFromHex(addrID)
+	if err != nil {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var deleted models.Address
+	err = addrColl.FindOneAndDelete(ctx, bson.M{"_id": id}).Decode(&deleted)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, "Address not found", http.StatusNotFound)
+			return
+		}
+		log.Println("DB error on Delete:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(deleted)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	var addr models.Address
+	if err := json.NewDecoder(r.Body).Decode(&addr); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	addr.ID = primitive.NewObjectID()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := addrColl.InsertOne(ctx, addr)
+	if err != nil {
+		log.Println("DB error on Create:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(addr)
+}
