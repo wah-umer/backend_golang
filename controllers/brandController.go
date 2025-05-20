63835d29c8a7a9d068f798a0f73803f83f187bca
wah-umer <umer.waheed@devsinc.com>
2025-05-21 01:04:55 +0500

    Created Brand and Category API

---
+++ controllers/brandController.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/umerwaheed/backend_golang/database"
+	"github.com/umerwaheed/backend_golang/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var brandColl = database.Collection("brands")
+
+func GetAllBrand(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cur, err := brandColl.Find(ctx, bson.M{})
+	if err != nil {
+		log.Println("DB error on Find brands:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(ctx)
+
+	var brands []models.Brand
+	cur.All(ctx, &brands)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(brands)
+}
---
+++ controllers/categoryController.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/umerwaheed/backend_golang/database"
+	"github.com/umerwaheed/backend_golang/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var catColl = database.Collection("brands")
+
+func GetAllCategory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cur, err := catColl.Find(ctx, bson.M{})
+	if err != nil {
+		log.Println("DB error on Find categories:", err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(ctx)
+
+	var categories []models.Category
+	cur.All(ctx, &categories)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(categories)
+}
